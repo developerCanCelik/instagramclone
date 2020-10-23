a633975dfdfee72f7bc806c3958ea3d0298372ff
cancelik-dev <64225167+cancelik-dev@users.noreply.github.com>
2020-10-23 18:02:14 +0300

    initial commit

--- app/src/main/java/com/cancelik/insatagramclone/HomeActivity.kt
+++ app/src/main/java/com/cancelik/insatagramclone/HomeActivity.kt
@@ -2,11 +2,12 @@ package com.cancelik.insatagramclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.ktx.Firebase
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
+        
     }
 }
